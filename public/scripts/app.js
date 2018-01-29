8165dac04b4a7ce581886b59db823b84737564d0
rbatsenko <rbatsenko@gmail.com>
2018-01-29 16:58:11 +0100

    REACT IS AWESOME

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -43,22 +43,50 @@ var template = React.createElement(
 );
 
 var count = 0;
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'Count: ',
-        count
-    ),
-    React.createElement(
-        'button',
-        { id: 'my-id', className: 'button' },
-        '+1'
-    )
-);
-console.log(templateTwo);
+var addOne = function addOne() {
+    count++;
+    renderCounterApp();
+};
+var minusOne = function minusOne() {
+    count--;
+    renderCounterApp();
+};
+var reset = function reset() {
+    count = 0;
+    renderCounterApp();
+};
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+var renderCounterApp = function renderCounterApp() {
+
+    var templateTwo = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            '+1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: minusOne },
+            '-1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'Reset'
+        )
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
--- src/README.md
+++
@@ -1,1 +0,0 @@
-React Indecision App
