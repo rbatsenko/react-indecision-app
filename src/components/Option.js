6978ce797dae808f48457a1226df7c952200182b
rbatsenko <rbatsenko@gmail.com>
2018-03-13 15:43:01 +0100

    more styles

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Option = (props) => (
     <div>
         <span>{props.optionText}</span>
-        <button 
+        <button
+            className='button button--link'
             onClick={(e) => {
                 props.handleDeleteOption(props.optionText);
             }}
